eb105dfd5e1927cd4ea4ee3cc7a468d77350e518
theinventor1 <37818640+theinventor1@users.noreply.github.com>
2023-06-22 01:42:07 -0400

    pull request 2
    pull request 2

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
+const { getCategorias } = require('../controllers/getCategoria.js');
 const { Router } = require('express');
 const router = Router();
 
-
+router.get("/categ",  getCategorias );
 
 
 module.exports = router ;
