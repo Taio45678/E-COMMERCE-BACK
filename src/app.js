c0ca699c2ff6e443e5b9975a2b19ad5fc4f4f48b
Taio45678 <107537078+Taio45678@users.noreply.github.com>
2023-06-26 16:59:11 -0300

    Sesiones

--- src/app.js
+++ src/app.js
@@ -4,25 +4,39 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-// post present
-
 const session = require('express-session');
-
-
-const { User }  = require('./db.js');
+const { User } = require('./db.js');
 
 const server = express();
+
 server.use(cors());
-server.name = 'API'; 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
- res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
- res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
- res.header('Access-Control-Allow-Credentials', 'true');
- next();
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
+server.use(session({
+  secret: 'mi_secreto',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+server.use(async (req, res, next) => {
+  if (req.session.userId) {
+    try {
+      const user = await User.findByPk(req.session.userId);
+      req.user = user;
+    } catch (error) {
+      console.error('Error al buscar el usuario:', error);
+    }
+  }
+  next();
 });
 
 server.use('/', routes);
