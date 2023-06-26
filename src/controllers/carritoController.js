73d01a01c5b000a5bd1d018ca07c0f9277604014
Taio45678 <taiogamer@gmail.com>
2023-06-26 20:45:48 -0300

    ups

--- src/controllers/carritoController.js
+++ src/controllers/carritoController.js
@@ -1,6 +1,6 @@
 const { CarroCompra, Usuario, Producto } = require('../db');
 
-// Controlador para obtener el carrito de compra
+// Controlador para obtener el carrito de compra de un usuario
 async function obtenerCarritoCompra(req, res) {
   const { idUsuario } = req.params;
 
@@ -16,10 +16,9 @@ async function obtenerCarritoCompra(req, res) {
         },
         {
           model: Usuario,
-          attributes: []
+          attributes: ['username', 'login']
         }
-      ],
-      attributes: [],
+      ]
     });
 
     res.json(carrito);
@@ -34,12 +33,24 @@ async function agregarProductoCarrito(req, res) {
   const { idUsuario, idProducto } = req.body;
 
   try {
-    const nuevoProductoCarrito = await CarroCompra.create({
+    // Verificar si el producto ya existe en el carrito
+    const productoExistente = await CarroCompra.findOne({
+      where: {
+        idusuario: idUsuario,
+        idproducto: idProducto
+      }
+    });
+
+    if (productoExistente) {
+      return res.status(400).json({ mensaje: 'El producto ya está en el carrito' });
+    }
+
+    const nuevoProducto = await CarroCompra.create({
       idusuario: idUsuario,
       idproducto: idProducto
     });
 
-    res.status(201).json(nuevoProductoCarrito);
+    res.status(201).json(nuevoProducto);
   } catch (error) {
     console.error(error);
     res.status(500).json({ mensaje: 'Error al agregar el producto al carrito de compra' });
@@ -51,13 +62,13 @@ async function eliminarProductoCarrito(req, res) {
   const { id } = req.params;
 
   try {
-    const productoCarrito = await CarroCompra.findByPk(id);
+    const producto = await CarroCompra.findByPk(id);
 
-    if (!productoCarrito) {
+    if (!producto) {
       return res.status(404).json({ mensaje: 'Producto del carrito no encontrado' });
     }
 
-    await productoCarrito.destroy();
+    await producto.destroy();
 
     res.json({ mensaje: 'Producto eliminado del carrito de compra correctamente' });
   } catch (error) {
