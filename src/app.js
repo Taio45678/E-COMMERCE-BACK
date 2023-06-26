6cd1e95473918658b3ce26b4be3da63090ee7818
Taio45678 <taiogamer@gmail.com>
2023-06-26 20:16:44 -0300

    Completado

--- src/app.js
+++ src/app.js
@@ -4,41 +4,34 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const session = require('express-session');
-const { User } = require('./db.js');
+// post present
 
+
+
+ require('./db.js');
 const server = express();
 
-server.use(cors());
+
+server.name = 'API'; 
+
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
-
-server.use(session({
-  secret: 'mi_secreto',
-  resave: false,
-  saveUninitialized: false,
-}));
-
-server.use(async (req, res, next) => {
-  if (req.session.userId) {
-    try {
-      const user = await User.findByPk(req.session.userId);
-      req.user = user;
-    } catch (error) {
-      console.error('Error al buscar el usuario:', error);
-    }
-  }
-  next();
+ res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+ res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+ res.header('Access-Control-Allow-Credentials', 'true');
+ next();
 });
 
 server.use('/', routes);
+
+server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
 
 module.exports = server;
