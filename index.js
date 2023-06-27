00b821e59f4f62aab405cefc6572e44480abd0ef
Taio45678 <taiogamer@gmail.com>
2023-06-27 09:12:48 -0300

    completo

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is listening on port ${PORT}`);
   });
+  
 });
