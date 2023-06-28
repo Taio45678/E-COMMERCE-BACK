69a60f9500445ae9cb23ceb06c81caf17e0fde8f
Taio45678 <taiogamer@gmail.com>
2023-06-28 16:46:26 -0300

    aaaaaaaaaaa

--- src/db.js
+++ src/db.js
@@ -3,6 +3,7 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_URL } = process.env;
+const {Carrocompra,Categoria,Fotoprod,Producto,Review,Usuario} = require("../models");
 
 const sequelize =
   process.env.NODE_ENV === "production"
