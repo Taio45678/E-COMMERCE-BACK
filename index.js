efd4e61aeb2e95c2fda3a188b11d10e918498960
Taio45678 <taiogamer@gmail.com>
2023-06-28 17:17:45 -0300

    sequelize primero

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const PORT = process.env.PORT;
-// Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is listening on port ${PORT}`);
+const { sequelize } = require('./src/db.js');
+const PORT = process.env.PORT || 3000;
+
+// Sincronizar los modelos con la base de datos
+sequelize.sync({ force: false })
+  .then(() => {
+    server.listen(PORT, '0.0.0.0', () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error al sincronizar la base de datos:', error);
   });
-  
-});
