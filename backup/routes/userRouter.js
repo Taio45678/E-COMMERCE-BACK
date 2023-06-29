8225c102a97ff0a2099ad33b4e27b13f0bc7b65f
Taio45678 <taiogamer@gmail.com>
2023-06-29 14:59:47 -0300

    Prueba definitiva

---
+++ backup/routes/userRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const autenticacionMiddleware = require('../middlewares/userAuth.js');
+
+// Ruta para obtener un usuario por su ID (requiere autenticación)
+
+
+
+
+module.exports = router;
