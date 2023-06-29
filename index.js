89cdb4e39202289895323c67ff26aaf7d5d12b36
Taio45678 <taiogamer@gmail.com>
2023-06-29 15:15:42 -0300

    aaaaa

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const server = require('./src/app.js');
 const { sequelize } = require('./src/db.js');
-const DB_PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sincronizar los modelos con la base de datos
 conn.sync({ force: true }).then(() => {
-  server.listen(DB_PORT, () => { console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => { console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
