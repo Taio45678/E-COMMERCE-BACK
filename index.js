a07b37dc94cfc6d1f1e5c8a449f3355ff1c3bea7
Taio45678 <taiogamer@gmail.com>
2023-06-29 15:20:37 -0300

    upsi

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const server = require('./src/app.js');
-const { sequelize } = require('./src/db.js');
-const PORT = process.env.PORT || 3000;
-
-// Sincronizar los modelos con la base de datos
+const { conn } = require('./src/db.js');
+const PORT = process.env.PORT
+// Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => { console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => { console.log('%s listening at PORT'); // eslint-disable-line no-console
   });
 });
