5f037b5c2aa9ea2a7e844bfb1e5ad3829311b437
Taio45678 <taiogamer@gmail.com>
2023-06-29 15:45:10 -0300

    app

--- src/app.js
+++ src/app.js
@@ -18,12 +18,13 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-    
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Origin', '*');
     next();
-   });
+  });
+  
 
 server.use('/', routes);
 
