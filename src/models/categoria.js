9b98a5c1c24dfcc9cf3b8021209d8fe21eaa8c76
taio45678 <taiogamer@gmail.com>
2023-06-30 11:34:52 -0300

    upsi

--- src/models/categoria.js
+++ src/models/categoria.js
@@ -3,6 +3,9 @@ module.exports = (sequelize) => {
  const Categoria = sequelize.define('categoria', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     nombrecat: { type: DataTypes.STRING, allowNull: false },
-  });
+  })
+  Categoria.associate = (models) => {
+    Categoria.hasMany(models.Producto, { foreignKey: 'categoriaId', allowNull: false });
+  };
   return Categoria;
 };
