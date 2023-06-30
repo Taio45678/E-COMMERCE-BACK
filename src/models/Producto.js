f528f8e47e3c59cfcda3dc6da9031f14af49f6d9
taio45678 <taiogamer@gmail.com>
2023-06-30 12:35:35 -0300

    dawfa

--- src/models/Producto.js
+++ src/models/Producto.js
@@ -2,24 +2,73 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
   const Producto = sequelize.define('producto', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    nombreproducto: { type: DataTypes.STRING, allowNull: true },
-    descproducto: { type: DataTypes.STRING, allowNull: true },
-    colorproducto: { type: DataTypes.ARRAY(DataTypes.STRING), allowNull: true },
-    fotoprinc: { type: DataTypes.STRING, allowNull: true },
-    precioproducto: { type: DataTypes.STRING, allowNull: true },
-    disponibproducto: { type: DataTypes.INTEGER, allowNull: true },
-    fotosecund: {
-      type: DataTypes.ARRAY(DataTypes.STRING), 
-      allowNull: true, 
-      defaultValue: [], 
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
     },
-    borrador: { type: DataTypes.BOOLEAN, allowNull: true, defaultValue: false },
+    nombreproducto: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'El nombre del producto no puede estar vacío.'
+        },
+        len: {
+          args: [3, 255],
+          msg: 'El nombre del producto debe tener entre 3 y 255 caracteres.'
+        }
+      }
+    },
+    descproducto: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    colorproducto: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: true
+    },
+    fotoprinc: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    precioproducto: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isDecimal: {
+          msg: 'El precio del producto debe ser un número decimal.'
+        },
+        min: {
+          args: 0,
+          msg: 'El precio del producto no puede ser negativo.'
+        }
+      }
+    },
+    disponibproducto: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        isInt: {
+          msg: 'La disponibilidad del producto debe ser un número entero.'
+        },
+        min: {
+          args: 0,
+          msg: 'La disponibilidad del producto no puede ser negativa.'
+        }
+      }
+    },
+    fotosecund: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: true,
+      defaultValue: []
+    },
+    borrador: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+      defaultValue: false
+    },
   });
-
-  Producto.associate = (models) => {
-    Producto.belongsTo(models.Categoria, { foreignKey: 'categoriaId', allowNull: false });
-  };
 
   return Producto;
 };
