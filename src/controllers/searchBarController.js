f96d1a392f53fbb35af73a1df8ae90fa4bb5b955
Taio45678 <taiogamer@gmail.com>
2023-06-30 19:58:05 -0300

    Al fin funcional

--- src/controllers/searchBarController.js
+++ src/controllers/searchBarController.js
@@ -2,29 +2,29 @@ const { Op } = require('sequelize');
 const db = require('../db.js');
 const buscarProductos = async (req, res) => {
   try {
-    const { query } = req.body;
-
+    //const { query } = req.body;
+    const prod = req.query.prod;
+    const cate = req.query.cate;
     const productos = await db.Producto.findAll({
       where: {
         [Op.or]: [
-          { nombreproducto: { [Op.iLike]: `%${query}%` } }
+          { nombreproducto: { [Op.iLike]: `%${prod}%` } }
         ]
       },
       include: {
         model: db.Categoria,
         where: {
-          nombrecat: { [Op.iLike]: `%${query}%` }
+          nombrecat: { [Op.iLike]: `%${cate}%` }
         },
         required: true
       }
     });
-
     res.json(productos);
   } catch (error) {
     console.error('Error al buscar productos:', error);
     res.status(500).json({ error: 'Error al buscar productos' });
   }
 };
-
-
 module.exports = buscarProductos;
+
+
