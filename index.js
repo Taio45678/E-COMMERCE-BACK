35931bc1e4165fd7c9935b00d86425c4300e8c4f
taio45678 <taiogamer@gmail.com>
2023-07-01 19:16:51 -0300

    aaaaa

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const { conn } = require('./src/db.js');
 const PORT = process.env.PORT
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(PORT, () => { console.log('%s listening at PORT'); // eslint-disable-line no-console
+  server.listen(3001, () => { console.log('%s listening at PORT'); // eslint-disable-line no-console
   });
 });
--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,14 +1,44 @@
 const { DataTypes } = require('sequelize');
+
 module.exports = (sequelize) => {
-const Usuario=  sequelize.define('usuario', {
-    id: {  type: DataTypes.INTEGER, autoIncrement: true,  primaryKey: true, },
-    login: { type: DataTypes.STRING,   allowNull: true,   unique: true, validate: {  isEmail: true, },  },
-    username: {  type: DataTypes.STRING,   allowNull: true,  }, password: { type: DataTypes.STRING, allowNull: true, },
-    telefono: {type: DataTypes.STRING, allowNull: true},
-    direccion: {type: DataTypes.STRING, allowNull:false},
-    rol: {   type: DataTypes.INTEGER,   allowNull: false,   validate: {  isIn: [[1, 2]], // 1 para usuario, 2 para admin
+  const Usuario = sequelize.define('Usuario', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    login: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
       },
-    },    
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    telefono: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    direccion: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    rol: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isIn: [[1, 2]], // 1 para usuario, 2 para admin
+      },
+    },
   });
+
   return Usuario;
 };
