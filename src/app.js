0e5c54390508f6a9d50ce621e154ccc770e4c9c9
Taio45678 <taiogamer@gmail.com>
2023-07-03 13:40:27 -0300

    Prueba Login

--- src/app.js
+++ src/app.js
@@ -1,31 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+require("dotenv").config();
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const { auth } = require('express-openid-connect');
 const routes = require('./routes/index.js');
-// post present
 
-
- //require('./db.js');
 const server = express();
 
-
-server.name = 'API'; 
+server.name = 'API';
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+server.use(cors());
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
-  
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'wS8b83wEOKDY4S9IzJgDEbRMWagqLX0V',
+  issuerBaseURL: 'https://dev-8zqxgygdfo4mmtdr.us.auth0.com'
+};
+
+server.use(auth(config));
 server.use('/', routes);
 
 // server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
