e4806870d768ed8097189781a0b0c4148b80818b
caparada1366 <ca.parada1366@gmail.com>
2023-07-05 23:18:59 -0500

    endpoint de filtros

--- src/controllers/searchBarController.js
+++ src/controllers/searchBarController.js
@@ -3,40 +3,99 @@ const db = require('../db.js');
 
 const buscarProductos = async (req, res) => {
   try {
-    const { prod, cate, page, limit } = req.query;
+    const { prod, cate, page, limit, color, price } = req.query;
     const pageNumber = parseInt(page) || 1;
     const pageSize = parseInt(limit) || 10;
     const offset = (pageNumber - 1) * pageSize;
-
-    const whereCondition = {
-      [Op.or]: [
-        { nombreproducto: { [Op.iLike]: `%${prod}%` } }
-      ]
-    };
-
-    if (cate) {
-      whereCondition['$Categoria.nombrecat$'] = { [Op.iLike]: `%${cate}%` };
+    const arrayCondiciones = []
+    var condicionCat =""
+    
+    const orden = []
+    if(prod){
+      arrayCondiciones.push({ nombreproducto: { [Op.iLike]: `%${prod}%` } })
+    }
+    if(cate){
+      condicionCat = {nombrecat : cate}
+    }
+    if(color){
+      arrayCondiciones.push({colorproducto: {[Op.overlap]: [color]}})
+    }
+    if(price){
+      orden.push('precioproducto')
+      if(price === "up") orden.push('ASC')
+      if(price === 'down') orden.push('DESC')
     }
 
-    const { count, rows } = await db.Producto.findAndCountAll({
-      where: whereCondition,
-      include: {
-        model: db.Categoria,
-        required: true
-      },
-      offset,
-      limit: pageSize,
-    });
+    if(price){
+      const { count, rows } = await db.Producto.findAndCountAll({
+        where: {
+          [Op.and]:
+          arrayCondiciones
 
-    const totalPages = Math.ceil(count / pageSize);
+        },
+        order: [
+          orden
+        ],
+        include: {
+          model: db.Categoria,
+          where: condicionCat,
+          attributes: ['nombrecat'],
+          through: {attributes: []},
+          required: true
+        },
+        offset,
+        limit: pageSize,
+      });
+      const totalPages = Math.ceil(count / pageSize);
+      const arrayRespuesta = []
+      rows.forEach(producto => {
+        const {id, nombreproducto, descproducto, colorproducto, fotoprinc, precioproducto, disponibproducto, categoria} = producto
+        var namecat = categoria[0].nombrecat
+        const productoFinal = {id, nombreproducto, descproducto, colorproducto, fotoprinc, precioproducto, disponibproducto, nombrecat: namecat}
+        arrayRespuesta.push(productoFinal)
+      });
 
-    res.json({
-      totalProductos: count,
-      totalPages,
-      currentPage: pageNumber,
-      pageSize,
-      productos: rows,
-    });
+      res.json({
+        totalProductos: count,
+        totalPages,
+        currentPage: pageNumber,
+        pageSize,
+        productos: arrayRespuesta,
+      });
+      console.log(JSON.stringify({
+        rows,
+      }))
+    }else {
+      const { count, rows } = await db.Producto.findAndCountAll({
+        where: {
+          [Op.and]:
+          arrayCondiciones
+        },
+        include: {
+          model: db.Categoria,
+          where: condicionCat,
+          attributes: ['nombrecat'],
+          through: {attributes: []},
+          required: true
+        },
+        offset,
+        limit: pageSize,
+      });
+      const totalPages = Math.ceil(count / pageSize);
+
+      res.json({
+        totalProductos: count,
+        totalPages,
+        currentPage: pageNumber,
+        pageSize,
+        productos: rows,
+      });
+      console.log(JSON.stringify({
+        rows,
+      }))
+    }
+
+    
   } catch (error) {
     console.error('Error al buscar productos:', error);
     res.status(500).json({ error: 'Error al buscar productos' });
