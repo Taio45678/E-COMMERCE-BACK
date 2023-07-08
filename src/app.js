d94f6ff890f24bc3f8938f33eab798bff00f2a23
Taio45678 <taiogamer@gmail.com>
2023-07-08 15:34:53 -0300

    Ojala funcione

--- src/app.js
+++ src/app.js
@@ -27,10 +27,10 @@ server.use((req, res, next) => {
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:3000',
-  clientID: 'wS8b83wEOKDY4S9IzJgDEbRMWagqLX0V',
-  issuerBaseURL: 'https://dev-8zqxgygdfo4mmtdr.us.auth0.com'
+  secret: SECRET_KEY,
+  baseURL: 'https://e-commerce-front-alpha.vercel.app/',
+  clientID: CLIENT_ID,
+  issuerBaseURL: AUTH_URL
 };
 
 server.use(auth(config));
