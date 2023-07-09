bdd50e36f689bce538c623e689c873964d2be2ff
Taio45678 <taiogamer@gmail.com>
2023-07-09 01:03:00 -0300

    agrego post oc

--- src/controllers/postOcDet.js
+++ src/controllers/postOcDet.js
@@ -4,28 +4,15 @@ const postOCyDetalle = async (req, res) => {
   const { loginuser, hashvalidacionpago, valortotaloc,estadooc, detalleocx } = req.body;
 
   try {
-    // Insertar en la tabla "oc"
-    const newOC = await Oc.create({
-      loginuser,
-      hashvalidacionpago,
-      valortotaloc,
-      estadooc
-    });
-
+   const fechahoraocaux = new Date();
+   const fechahoraoc = fechahoraocaux.toISOString();
+    const newOC = await Oc.create({  fechahoraoc, loginuser, hashvalidacionpago, valortotaloc, estadooc });
     // Obtener el idoc generado para la OC recién insertada
     const idoc = newOC.idoc;
-
     // Insertar en la tabla "detalleoc" por cada objeto en "detalleocx"
     for (let i = 0; i < detalleocx.length; i++) {
-      const { idproducto, valorunitario, cant, subtotal } = detalleocx[i];
-
-      await Detalleoc.create({
-        idoc,
-        idproducto,
-        valorunitario,
-        cant,
-        subtotal
-      });
+      const { idproducto, nombreproducto, valorunitario, cant, subtotal } = detalleocx[i];
+      await Detalleoc.create({ idoc, idproducto, nombreproducto, valorunitario, cant, subtotal });
     }
 
     return res.status(201).send(newOC);
