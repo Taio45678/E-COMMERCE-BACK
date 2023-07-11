1d4353c792bac30d680970d614f1d1ac19879e08
Taio45678 <taiogamer@gmail.com>
2023-07-10 23:09:38 -0300

    a ver

--- src/models/oc.js
+++ src/models/oc.js
@@ -7,7 +7,15 @@ module.exports = (sequelize) => {
   hashvalidacionpago: { type: DataTypes.STRING, allowNull: true, },
   loginuser: { type: DataTypes.STRING, allowNull: true,  },
   valortotaloc: { type: DataTypes.INTEGER, allowNull: true,  },
-  estadooc: { type: DataTypes.STRING, allowNull: true,  }
-  });
+  estadooc: { type: DataTypes.STRING, allowNull: true,  },
+  usuarioId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: {
+      model: Usuario,
+      key: 'id', // La columna de la tabla "usuarios" que se utilizará para la relación
+    },
+  }});
+  
   return Oc;
 };
