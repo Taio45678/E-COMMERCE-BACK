a8f3049a57cd78c5635711524fa98bfe338f278a
Taio45678 <107537078+Taio45678@users.noreply.github.com>
2023-07-11 20:40:59 -0300

    a

--- src/controllers/postOcDet.js
+++ src/controllers/postOcDet.js
@@ -5,6 +5,7 @@ const { Oc, Detalleoc } = require('../db');
 const { createPaymentPreference } = require('./payamentController.js');
 
 const dummy1 = { "loginuser":"felipejob1@yahoo.com", "idoc":2 };
+
 const postOCyDetalle = async (req, res) => {
   const { loginuser, hashvalidacionpago, estadooc, detalleocx } = req.body;
   let valortotaloc = 0;
@@ -25,6 +26,16 @@ const postOCyDetalle = async (req, res) => {
 
       valortotaloc += subtotal; // Sumar el subtotal al valortotaloc
     }
+
+    console.log('newOC: ', newOC);
+    console.log('idoc: ', newOC.idoc);
+
+    /** */
+    const urlx = 'https://commerce-back-2025.up.railway.app/create-order';
+    const bodyx = {
+      loginuserparam: loginuser,
+      idocparam: idoc
+    };
 
     
     console.log('newOC: ', newOC);
