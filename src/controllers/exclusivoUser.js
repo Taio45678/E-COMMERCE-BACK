5fd95fd82cfe60d82f9f5fec528185ab068463db
Taio45678 <107537078+Taio45678@users.noreply.github.com>
2023-07-12 20:24:05 -0300

    A

--- src/controllers/exclusivoUser.js
+++ src/controllers/exclusivoUser.js
@@ -13,6 +13,7 @@ const paginadoUserOc = async (req, res) => {
 
     // Obtener todas las OCs con estadooc "aprobado" o "fallido" y sus detalles y el número total de OCs
     const { count, rows } = await Oc.findAndCountAll({
+      distinct:true,
       offset,
       limit: limitNumber,
       include: [Detalleoc], // Asegúrate de incluir el modelo Detalleoc correctamente
