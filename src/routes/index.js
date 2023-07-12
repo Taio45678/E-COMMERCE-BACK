8875d97b9589a01cb80e0acd773ba82be1659729
Taio45678 <107537078+Taio45678@users.noreply.github.com>
2023-07-12 15:29:07 -0300

    A

--- src/routes/index.js
+++ src/routes/index.js
@@ -57,7 +57,7 @@ router.post('/usuarios/:idUsuario/carritoCrear', /*auth(config),*/ agregarProduc
 router.delete('/usuarios/:idUsuario/carrito/:idProducto', /*auth(config),*/ eliminarProductoCarrito);
 
 //este es la ruta para auth0 se supone
-router.patch('/users/:id/blocked', userController.updateUserBlockedStatus);
+router.patch('/users/:id/blocked', updateUserBlockedStatus);
 router.put('/usuarios/:id/isban', actualizarIsBan);
 router.put('/usuarios/:id/rol', actualizarRol);
 router.get('/usuarios/:userId', obtenerDatosUsuario);
